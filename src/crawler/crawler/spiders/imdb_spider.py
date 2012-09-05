3de734984927b1e44d623bb9e346a47371a3a353
Daniel Sarmiento <dsarmientos@gmail.com>
2012-09-05 09:43:08 -0500

    Born today for wednesday september 5

--- src/crawler/crawler/spiders/imdb_spider.py
+++ src/crawler/crawler/spiders/imdb_spider.py
@@ -16,11 +16,11 @@ class ImdbSpider(CrawlSpider):
     name = "imdb"
     allowed_domains = ["www.imdb.com"]
     start_urls = [
-        'http://www.imdb.com/name/nm0461498/',
-        'http://www.imdb.com/name/nm0004747/',
-        'http://www.imdb.com/name/nm0001834/',
-        'http://www.imdb.com/name/nm0018554/',
-        'http://www.imdb.com/name/nm0412382/',
+        'http://www.imdb.com/name/nm0001348/',
+        'http://www.imdb.com/name/nm0000535/',
+        'http://www.imdb.com/name/nm0627878/',
+        'http://www.imdb.com/name/nm0000474/',
+        'http://www.imdb.com/name/nm0000079/',
     ]
     rules = (
         # Extract links matching and parse them with the spider's method
