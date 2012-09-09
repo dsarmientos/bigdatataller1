22b1f6e8a152e669f4ed26dc52c9fdd07d71e651
Daniel Sarmiento <dsarmientos@gmail.com>
2012-09-09 13:59:37 -0500

    More fields being parsed

--- src/mapreduce/imdb.py
+++ src/mapreduce/imdb.py
@@ -9,7 +9,7 @@ def html_mapper(key, value):
     yield value, html
 
 def reducer(key, value):
-    html = list(value)[0]
+    html = value.next()
     yield key, html
 
 class ActorMapper(object):
@@ -28,6 +28,23 @@ class ActorMapper(object):
         if birthday_tag:
            birthday = birthday_tag.get('datetime')
            actor['birthday'] = birthday
+        birth_place_tag = soup.find('a', href=re.compile('\?birth_place=.+'))
+        if birth_place_tag:
+            birth_place = birth_place_tag.text.split(',')
+            if birth_place:
+                actor['country'] = birth_place[-1].strip()
+        job_title_tags = soup.find_all('a', itemprop='jobTitle')
+        job_titles = [tag.text.lower() for tag in job_title_tags]
+        gender = '?'
+        if 'actor' in job_titles:
+            gender = 'M'
+        elif 'actress' in job_titles:
+            gender = 'F'
+        # finally, guess director means male
+        elif 'director' in job_titles:
+            gender = 'M'
+        actor['gender'] = gender
+
         return actor
 
 
