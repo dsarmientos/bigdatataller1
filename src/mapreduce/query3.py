5a6ebc9a4f37265082ca1eae8f5317f1fce525c3
Daniel Sarmiento <dsarmientos@gmail.com>
2012-09-10 06:40:42 -0500

    Queries donde

--- src/mapreduce/query3.py
+++ src/mapreduce/query3.py
@@ -9,9 +9,7 @@ class ActorFilterMapper(object):
     def __call__(self, key, value):
         id_, val = value.split('\t')
         record = ast.literal_eval(val)
-        fields = ('birthday')
-        has_fields = all([record.has_key(field) for field in fields])
-        if has_fields and record.get('record_type', '') == 'actor':
+        if record.get('record_type', '') == 'actor' and 'birthday' in record:
             yield id_, record
 
 
@@ -20,9 +18,13 @@ class ActorQueryMapper(object):
         date = self.params["date"]
         self.date = datetime.datetime.strptime(date,'%Y-%m-%d')
     def __call__(self, key, value):
-        birthday = datetime.datetime.strptime(value['birthday'],'%Y-%m-%d')
-        if self.date.month == birthday.month and self.date.day == birthday.day:
-            yield key, str(value)
+        try:
+            birthday = datetime.datetime.strptime(value['birthday'],'%Y-%m-%d')
+        except ValueError:
+            pass
+        else:
+            if self.date.month == birthday.month and self.date.day == birthday.day:
+                yield key, str(value)
 
 
 if __name__ == "__main__":
